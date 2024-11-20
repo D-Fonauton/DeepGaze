a11d73cbdb22524c4811b4fb38f771c63cff26f3
Buxin Liao <buxin.liao@mail.mcgill.ca>
2024-11-20 16:43:06 -0500

    update compute-canada related files

---
+++ deepgazeiie.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1       # Request GPU "generic resources"
+#SBATCH --cpus-per-task=6  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
+#SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --time=0-03:00
+#SBATCH --output=%N-%j.out
+
+module load ipython-kernel/3.10 # Make sure to choose a version that suits your application
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index -r requirements.txt
+
+python deepgazeiie.py
